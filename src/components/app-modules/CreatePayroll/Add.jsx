413ba6a4fc73020d17272f3ffb37c71bdc929b3a
Md. Jiaur Rahman <jiaur.rahman@apisolutionsltd.com>
2024-09-22 17:21:48 +0600

    modal fix

--- src/components/app-modules/CreatePayroll/Add.jsx
+++ src/components/app-modules/CreatePayroll/Add.jsx
@@ -3,7 +3,7 @@ import { DateInput } from "@mantine/dates";
 import {
   Modal,
   TextInput,
-  Textarea,
+  Fieldset,
   Button,
   Select,
   Group,
@@ -17,74 +17,91 @@ const Index = ({ opened, close }) => {
       <Modal
         classNames={{
           title: "modalTitle",
+          header: "modalHeader",
         }}
         opened={opened}
         title="Create Payroll"
         onClose={close}
         centered
+        size="xl"
+        padding="40px"
       >
         <form>
-          <Grid>
+          <Grid classNames={{ root: "gutterX", col: "gutterCol" }}>
             <Grid.Col span={{ base: 12, lg: 6 }}>
-              <TextInput mb="sm" label="Title" placeholder="Title" />
+              <TextInput label="Title" placeholder="Title" />
+            </Grid.Col>
+            <Grid.Col span={{ base: 12, lg: 6 }}>
               <DateInput
-                mb="sm"
                 valueFormat="DD MMM YYYY"
                 label="Posting Date"
                 placeholder="DD MMM YYYY"
               />
+            </Grid.Col>
+            <Grid.Col span={{ base: 12, lg: 6 }}>
               <DateInput
-                mb="sm"
                 valueFormat="DD MMM YYYY"
                 label="From Date"
                 placeholder="DD MMM YYYY"
               />
+            </Grid.Col>
+            <Grid.Col span={{ base: 12, lg: 6 }}>
               <DateInput
-                mb="sm"
                 valueFormat="DD MMM YYYY"
                 label="To Date"
                 placeholder="DD MMM YYYY"
               />
-              <Select
-                label="Company"
-                placeholder="Pick value"
-                data={["company_id-A", "company_id-B"]}
-              />
             </Grid.Col>
             <Grid.Col span={{ base: 12, lg: 6 }}>
               <Select
-                mb="sm"
-                label="Branch"
-                placeholder="Pick value"
-                data={["branch_id-A", "branch_id-B"]}
-              />
-              <Select
-                mb="sm"
-                label="Department"
-                placeholder="Pick value"
-                data={["department_id-A", "department_id-B"]}
-              />
-              <Select
-                mb="sm"
-                label="Employee Grade"
-                placeholder="Pick value"
-                data={["Grade-A", "Grade-B"]}
-              />
-              <Select
-                mb="sm"
-                label="Designation"
-                placeholder="Pick value"
-                data={["Front-End Developer", "Back-End Developer"]}
-              />
-              <Select
                 label="Effective Bank"
-                placeholder="Pick value"
+                placeholder="Effective Bank"
                 data={["Bank-A", "Bank-B"]}
               />
             </Grid.Col>
           </Grid>
 
-          <Group mt="sm" justify="flex-end">
+          <Fieldset mt={"xl"} className="fieldsetCus">
+            <Grid classNames={{ root: "gutterX", col: "gutterCol" }}>
+              <Grid.Col span={{ base: 12, lg: 6 }}>
+                <Select
+                  label="Company"
+                  placeholder="Company"
+                  data={["company_id-A", "company_id-B"]}
+                />
+              </Grid.Col>
+              <Grid.Col span={{ base: 12, lg: 6 }}>
+                <Select
+                  label="Branch"
+                  placeholder="Branch"
+                  data={["branch_id-A", "branch_id-B"]}
+                />
+              </Grid.Col>
+              <Grid.Col span={{ base: 12, lg: 6 }}>
+                <Select
+                  label="Department"
+                  placeholder="Department"
+                  data={["department_id-A", "department_id-B"]}
+                />
+              </Grid.Col>
+              <Grid.Col span={{ base: 12, lg: 6 }}>
+                <Select
+                  label="Employee Grade"
+                  placeholder="Employee Grade"
+                  data={["Grade-A", "Grade-B"]}
+                />
+              </Grid.Col>
+              <Grid.Col span={{ base: 12, lg: 6 }}>
+                <Select
+                  label="Designation"
+                  placeholder="Designation"
+                  data={["Front-End Developer", "Back-End Developer"]}
+                />
+              </Grid.Col>
+            </Grid>
+          </Fieldset>
+
+          <Group mt="xl" justify="flex-end">
             <Button type="submit">Save</Button>
           </Group>
         </form>
